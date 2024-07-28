fdc160474153b2c97a79c77dd6e86859d432afa1
dpavon-g <pavongomezdaniel@gmail.com>
2024-07-28 17:35:52 +0200

    Diseño mas bonito del entorno

---
+++ public/js/import3D.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+console.log('Importado');
